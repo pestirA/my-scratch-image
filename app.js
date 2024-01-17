9e4c3e71488678176cfbd6ee0087502cecd3119f
E. Pauline Abishai <35427591+pestirA@users.noreply.github.com>
2024-01-17 16:59:25 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
